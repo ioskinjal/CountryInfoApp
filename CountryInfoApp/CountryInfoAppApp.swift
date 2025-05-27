38f0212d908ecde7713bb31486b193ae1ee639e1
Kinjal Gadhia <ioskinjal@gmail.com>
2025-05-27 15:16:01 +0530

    Initial Commit

---
+++ CountryInfoApp/CountryInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryInfoAppApp.swift
+//  CountryInfoApp
+//
+//  Created by Totochan on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
